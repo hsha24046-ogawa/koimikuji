1901573a76606df62d0c26135e8663ecce8df54f
hsha24046-ogawa <hsha24046@ict.shimanet.ed.jp>
2026-05-01 12:11:32 +0900

    大凶追加

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const fortunes = [
     "中吉　少し勇気を出すといいことが起きる",
     "小吉　今日はゆっくり過ごそう",
     "凶　無理せず慎重に行動しよう",
+    "大凶　きおつけて"
 ];
 
 const button = document .getElementById("a");
@@ -11,6 +12,14 @@ const result = document .getElementById("q");
 button.addEventListener("click",function () {
 
     const randomNumber = Math.floor(Math.random() * fortunes.length)
+    if (randomNumber==0) {
+        result.style.color = "red";
+    }
+    else
+        {
+        result.style.color = "black";
+        
 
+    }
     result.textContent = fortunes[(randomNumber)];
 });
